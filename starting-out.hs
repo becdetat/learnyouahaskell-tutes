3d6bd3b234f57e760d445c9dc97887d362ec9363
Ben Scott <ben@belfryimages.com.au>
2013-03-03 00:10:03 +1000

    name generator

--- starting-out.hs
+++ starting-out.hs
@@ -14,3 +14,6 @@ doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 
 boomBangs numbers = [ if x < 10 then "BOOM" else "BANG" | x <- numbers, odd x ]
 
+nouns = ["hobo","frog","pope"]
+adjectives = ["lazy","grouchy","scheming"]
+nameGenerator = [ adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns ]
